f76f11ddd8db6bde1e400940e6fa724d4e5760b5
Henry Cui <z.henry.cui@gmail.com>
2021-03-15 11:27:48 +0900

    more optimizers

--- detectron2/solver/build.py
+++ detectron2/solver/build.py
@@ -121,14 +121,25 @@ def build_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimiz
         bias_lr_factor=cfg.SOLVER.BIAS_LR_FACTOR,
         weight_decay_bias=cfg.SOLVER.WEIGHT_DECAY_BIAS,
     )
-    return maybe_add_gradient_clipping(cfg, torch.optim.SGD)(
-        params,
-        lr=cfg.SOLVER.BASE_LR,
-        momentum=cfg.SOLVER.MOMENTUM,
-        nesterov=cfg.SOLVER.NESTEROV,
-        weight_decay=cfg.SOLVER.WEIGHT_DECAY,
-    )
 
+    if cfg.SOLVER.OPTIMIZER == 'sgd':
+        return maybe_add_gradient_clipping(cfg, torch.optim.SGD)(
+            params,
+            lr=cfg.SOLVER.BASE_LR,
+            momentum=cfg.SOLVER.MOMENTUM,
+            nesterov=cfg.SOLVER.NESTEROV,
+            weight_decay=cfg.SOLVER.WEIGHT_DECAY,
+        )
+
+    elif cfg.SOLVER.OPTIMIZER == 'adam':
+        return maybe_add_gradient_clipping(cfg, torch.optim.Adam)(
+            params,
+            lr=cfg.SOLVER.BASE_LR,
+            weight_decay=cfg.SOLVER.WEIGHT_DECAY,
+        )
+
+    else:
+        raise
 
 def get_default_optimizer_params(
     model: torch.nn.Module,
